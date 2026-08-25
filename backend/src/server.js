32df2c83af83d933d1dc7dc7cef44a6a3ab9c1fa
Ritik varun <132301755+ritikvarun@users.noreply.github.com>
2026-08-25 20:25:48 +0530

    new add

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,20 @@
-import express from "express"
-import dotenv from "dotenv"
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-import authRoutes from "./routes/auth.js"
-import messageRoutes from "./routes/message.js"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 app.use(express.json());
 
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
     console.log(`Hello Server Running on http://localhost:${PORT}`);
+    connectDB();
 });
